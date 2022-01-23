d66519539a458fd483249ac0a81dfd5e4988e78a
Perry Xie <perry.xie1@gmail.com>
2022-01-22 16:49:10 -0800

    Add LoginFormContainer

--- frontend/components/session_form/SignupFormContainer.jsx
+++ frontend/components/session_form/SignupFormContainer.jsx
@@ -1,6 +1,6 @@
 import { connect } from "react-redux";
+import { signup } from "../../actions/session_actions";
 import SessionForm from './SessionForm';
-import { signup } from "../../util/session_api_util";
 
 const mapStateToProps = (state) => {
   return {
