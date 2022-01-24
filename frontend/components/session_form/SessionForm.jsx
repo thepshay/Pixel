784f35da88a9f1cd3ec11bfbd35c512e7de79533
Perry Xie <perry.xie1@gmail.com>
2022-01-23 16:12:26 -0800

    Style sign in page

--- frontend/components/session_form/SessionForm.jsx
+++ frontend/components/session_form/SessionForm.jsx
@@ -17,28 +17,29 @@ class SessionForm extends React.Component{
 
   render() {
     const userForm = this.props.formType === 'signup' ? {
-      action: 'Sign Up',
-      className: 'signup-container'
+      action: 'CREATE YOUR ACCOUNT',
+      className: 'signup-container',
+      submitText: 'Login'
     } : {
-      action: 'Sign In',
-      className: 'login-container'
+      action: 'SIGN IN',
+      className: 'login-container',
+      submitText: 'Sign In'
     }
 
     return(
       <div className={userForm.className}>
-        <h1>{userForm.action}</h1>
+        <h1 className="title">{userForm.action}</h1>
         <form>
-        <div className="username-input">
-          <div>Pixel account name</div>
-          <input id='username' type='text' value={this.state.username} onChange={this.handleChange('username')}/>
-        </div>
-        <div className="password-input">
-          <div>Password</div>
-          <input id='password' type='password' value={this.state.password} onChange={this.handleChange('password')}/>
-        </div>
-        {/* <button type='submit'>{userForm.action}</button> */}
+          <div className="username-input">
+            <div>Pixel account name</div>
+            <input id='username' type='text' value={this.state.username} onChange={this.handleChange('username')}/>
+          </div>
+          <div className="password-input">
+            <div>Password</div>
+            <input id='password' type='password' value={this.state.password} onChange={this.handleChange('password')}/>
+          </div>
         </form>
-        <div className='submit-btn' onClick={(e)=>this.handleSubmit(e)}>{userForm.action}</div>
+        <div className='submit-btn' onClick={(e)=>this.handleSubmit(e)}>{userForm.submitText}</div>
         <ul>
           {this.props.errors.map((error, index) => <li key={index}>{error}</li>)}
         </ul>
