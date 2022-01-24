4c34fea28e8f1db99f910a952e8040f1440a6e1e
Perry Xie <perry.xie1@gmail.com>
2022-01-24 15:03:00 -0800

    Update image loading

--- frontend/components/session_form/LoginInfoBox.jsx
+++ frontend/components/session_form/LoginInfoBox.jsx
@@ -6,7 +6,7 @@ const LoginInfoBox = (props) => {
   return (
     <div className="login-info-container">
       <p>Join Pixel and discover tens of games to play.</p>
-      <img className='join-img' src={pic} />
+      <img className='join-img' src={window.joinPc} />
       <p>It's free and easy to use.</p>
       <Link to='/signup' replace>
         <div className="signup-btn">Join Pixel</div>
