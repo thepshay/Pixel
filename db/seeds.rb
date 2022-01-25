6612915f3899648cef9ee94e3ac8c2dd0dcd6870
Perry Xie <perry.xie1@gmail.com>
2022-01-24 21:28:01 -0800

    Seed first profile picture

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,7 @@ User.create([
   {username: 'user4', password:'123456'},
   {username: 'user5', password:'123456'}
 ])
+
+user1 = User.find(1);
+file = open('https://wiki.teamfortress.com/w/images/5/57/Buffed_blu_soldier.jpg')
+user1.photo.attach(io: file, filename: 'blu_soldier.jpg')
