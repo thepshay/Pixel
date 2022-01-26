5b48ae02a86fcac23123001b5424c8dc04db67fd
Perry Xie <perry.xie1@gmail.com>
2022-01-26 15:35:26 -0800

    Add CarouselContainer

---
+++ frontend/components/carousel/FeatureCarouselContainer.jsx
@@ -0,0 +1,20 @@
+import { connect } from 'react-redux';
+import fetchAllGames from '../../actions/game_actions'
+import { Carousel } from './Carousel';
+
+FEATURE_ID = [1,2,3,4,5,6,7]
+
+const mapStateToProps = (state) => {
+  return {
+    games: state.entities.games,
+    featuredGameId: FEATURE_ID
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return ({
+    fetchAllGames: () => dispatch(fetchAllGames())
+  })
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Carousel)
