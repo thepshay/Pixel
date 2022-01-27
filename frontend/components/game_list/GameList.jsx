e257de0d1eac9949cae7c613e253dcff9521f3ad
Perry Xie <perry.xie1@gmail.com>
2022-01-27 15:11:44 -0800

    Made game list files

---
+++ frontend/components/game_list/GameList.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class GameList extends React.Component {
+
+  render() {
+    return (
+      <div>
+        item
+      </div>
+    )
+  }
+}
+
+export default GameList;
---
+++ frontend/components/game_list/GameListItem.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class GameListItem extends React.Component {
+
+  render() {
+    return (
+      <div>
+        item
+      </div>
+    )
+  }
+}
+
+export default GameListItem;
