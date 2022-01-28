22b56e6fe7c2f94db8b170e215ec0f6d92f0df07
Perry Xie <perry.xie1@gmail.com>
2022-01-28 10:29:57 -0800

    Add InfoDisplay elements

--- frontend/components/game_list/InfoDisplay.jsx
+++ frontend/components/game_list/InfoDisplay.jsx
@@ -6,7 +6,24 @@ class InfoDisplay extends React.Component {
     const {game} = this.props;
 
     return (
-      <div>{game.title}</div>
+      <div>
+        <div>{game.title}</div>
+        <ul>
+          <li>{game.genre_1}</li>
+          <li>{game.genre_2}</li>
+        </ul>
+        <ul>
+          {game.photoUrls.slice(0, 4).map((screenshot, index) => {
+            return (
+              <img
+                className='display-screenshot' 
+                src={screenshot} 
+                key={game.id+index} 
+              />
+            )
+          })}
+        </ul>
+      </div>
     )
   }
 }
