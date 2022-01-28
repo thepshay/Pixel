7a94d09a3964ec4ff713927947aab77987b68a75
Perry Xie <perry.xie1@gmail.com>
2022-01-28 14:45:12 -0800

    Style genresidebar

--- frontend/components/Home/HomePage.jsx
+++ frontend/components/Home/HomePage.jsx
@@ -9,17 +9,20 @@ class HomePage extends React.Component {
   render() {
     return (
       <div className='home-page-container'>
-        <GenreSidebarContainer />
-        <CarouselContainer 
-          title={'FEATURE & RECOMMENDED'}
-          className={'carousel home-carousel'}
-          itemClassName={'home-carousel-item'}
-          displayGameId={[1,4,13,7,9,17,6]}
-        />
-        <GameListContainer 
-          className={'game-list home-game-list'}
-          displayGameId={ [2,3,5,8,10,11,12,14,15,16] }
-        />
+
+        <div className='main'>
+          <GenreSidebarContainer />
+          <CarouselContainer 
+            title={'FEATURE & RECOMMENDED'}
+            className={'carousel home-carousel'}
+            itemClassName={'home-carousel-item'}
+            displayGameId={[1,4,13,7,9,17,6]}
+          />
+          <GameListContainer 
+            className={'game-list home-game-list'}
+            displayGameId={ [2,3,5,8,10,11,12,14,15,16] }
+          />
+        </div>
         <Footer />
       </div>
     )
--- frontend/components/genre/GenreSidebar.jsx
+++ frontend/components/genre/GenreSidebar.jsx
@@ -13,7 +13,7 @@ class GenreSidebar extends React.Component {
 
     return (
       <div className="genre-sidebar">
-        <img className='steam-cash-img' src={window.steamWallet} />
+        <img className='steam-cash-img' src={window.steamCash} />
         <h2 className="genre-title">BROWSE BY GENRE</h2>
         <ul className="genre-list">
           {genres.map((genre, index) => {
