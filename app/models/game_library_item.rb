0d89ff78fc4e4cfb6c2112d86b34d23840bd8d92
Perry Xie <perry.xie1@gmail.com>
2022-02-01 15:03:50 -0800

    Add game library item model

--- app/models/game_library_item.rb
+++ app/models/game_library_item.rb
@@ -1,2 +1,6 @@
 class GameLibraryItem < ApplicationRecord
+  validates :user_id, :game_id, presence: true
+  validates :game_id, uniqueness: {scope: :user_id, message: 'is already in library'}
+  belongs_to :user
+  belongs_to :game
 end
