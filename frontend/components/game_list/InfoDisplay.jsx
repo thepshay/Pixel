9452e9896f680fd2601ccdd7fb171e5d2b822981
Perry Xie <perry.xie1@gmail.com>
2022-01-31 17:25:16 -0800

    Change genre to title form

--- frontend/components/game_list/InfoDisplay.jsx
+++ frontend/components/game_list/InfoDisplay.jsx
@@ -2,6 +2,17 @@ import React from 'react';
 
 class InfoDisplay extends React.Component {
 
+  changeToTitle(str) {
+    const words = str.split('-');
+    return words.map(word => {
+      return this.capitalize(word)
+    }).join(' ');
+  }
+
+  capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
   render() {
     const {game, className} = this.props;
 
@@ -9,8 +20,8 @@ class InfoDisplay extends React.Component {
       <div className={className}>
         <h3 className='info-title'>{game.title}</h3>
         <ul className='genre-list'>
-          <li className='genre'>{game.genre_1}</li>
-          <li className='genre'>{game.genre_2}</li>
+          <li className='genre'>{this.changeToTitle(game.genre_1)}</li>
+          <li className='genre'>{this.changeToTitle(game.genre_2)}</li>
         </ul>
         {game.photoUrls.slice(0, 4).map((screenshot, index) => {
           return (
--- frontend/components/show_page/GameInfo.jsx
+++ frontend/components/show_page/GameInfo.jsx
@@ -2,6 +2,17 @@ import React from "react";
 
 class GameInfo extends React.Component {
 
+  changeToTitle(str) {
+    const words = str.split('-');
+    return words.map(word => {
+      return this.capitalize(word)
+    }).join(' ');
+  }
+
+  capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
   render() {
     const {game} = this.props;
 
@@ -12,8 +23,8 @@ class GameInfo extends React.Component {
         <div className="genre-container">
           <h5>Popular user-defined tags for this product:</h5>
           <ul className='genre-list'>
-            <li className='genre'>{game.genre_1}</li>
-            <li className='genre'>{game.genre_2}</li>
+            <li className='genre'>{this.changeToTitle(game.genre_1)}</li>
+            <li className='genre'>{this.changeToTitle(game.genre_2)}</li>
           </ul>
         </div>
       </div>
