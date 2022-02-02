40793ecf494468127a9c3b16767594124a4769df
Perry Xie <perry.xie1@gmail.com>
2022-02-01 21:49:17 -0800

    Add user reducers

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -4,12 +4,14 @@ import cartItemReducer from "./cart_item_reducer";
 import gameReducer from "./game_reducer";
 import genreReducer from "./genre_reducer";
 import libraryItemReducer from "./library_item_reducer";
+import userReducer from "./user_reducer";
 
 const entitiesReducer = combineReducers({
   games: gameReducer,
   genre: genreReducer,
   cart: cartItemReducer,
-  library: libraryItemReducer
+  library: libraryItemReducer,
+  users: userReducer
 })
 
 export default entitiesReducer;
