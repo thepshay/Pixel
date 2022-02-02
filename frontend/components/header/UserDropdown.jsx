8c9fbba6a703a5d79fd81d14a5d34383bce494ea
Perry Xie <perry.xie1@gmail.com>
2022-02-01 17:22:52 -0800

    Add library and cart links to dropdown

--- frontend/components/header/UserDropdown.jsx
+++ frontend/components/header/UserDropdown.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
 class UserDropdown extends React.Component {
   
@@ -8,10 +9,15 @@ class UserDropdown extends React.Component {
 
     return (
       <ul className={className}>
+        <Link to='/cart'>
+          <li>Cart</li>
+        </Link>
+        <Link to='/library'>
+          <li>Library</li>
+        </Link>
         <li onClick={()=>logout(currentUser.id)}>
           Logout: <span>{currentUser.username}</span>
         </li>
-
       </ul>
     )
   }
