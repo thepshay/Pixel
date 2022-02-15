dcd4515880c88c114d18ece40b7a84e4c0c3152c
Perry Xie <perry.xie1@gmail.com>
2022-02-15 15:38:58 -0800

    Add review error reducer;

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -5,12 +5,14 @@ import sessionReducer from "./session_reducer";
 import sessionErrorsReducer from "./session_errors_reducer";
 import cartItemErrorsReducer from "./cart_item_errors_reducer";
 import libraryItemErrorsReducer from "./library_item_errors_reducer";
+import reviewErrorsReducer from "./review_errors_reducer";
 
 const rootReducer = combineReducers({
   entities: entitiesReducer,
   errors: sessionErrorsReducer,
   cartErrors: cartItemErrorsReducer,
   libraryErrors: libraryItemErrorsReducer,
+  reviewErrors: reviewErrorsReducer,
   session: sessionReducer
 });
 
