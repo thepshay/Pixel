50ac9b1358ed5840d817bb30c0889191419e68f2
Perry Xie <perry.xie1@gmail.com>
2022-02-15 15:30:59 -0800

    Add error handling to review actions

--- frontend/actions/review_actions.js
+++ frontend/actions/review_actions.js
@@ -1,5 +1,6 @@
 export const RECEIVE_ALL_REVIEWS = 'RECEIVE_ALL_REVIEWS';
 export const RECEIVE_REVIEW = 'RECEIVE_REVIEW';
+export const RECEIVE_REVIEW_ERRORS = 'RECEIVE_REVIEW_ERRORS'
 
 import * as ReviewApiUtil from '../util/review_api_utils';
 
@@ -17,31 +18,46 @@ const receiveReview = (review) => {
   }
 }
 
+const receiveReviewErrors = (errors) => {
+  return {
+    type: RECEIVE_REVIEW_ERRORS,
+    errors
+  }
+}
+
 export const fetchAllReviews = (gameId) => (dispatch) => {
   // debugger
   return ReviewApiUtil.fetchAllReviews(gameId)
     .then((reviews) => {
       dispatch(receiveAllReviews(reviews))
-    })
+    }, errors => {
+      dispatch(receiveReviewErrors(errors.responseJSON))
+    });
 }
 
 export const createReview = (review) => (dispatch) => {
   return ReviewApiUtil.postReview(review)
     .then((payload) => {
       dispatch(receiveReview(payload))
-    })
+    }, errors => {
+      dispatch(receiveReviewErrors(errors.responseJSON))
+    });
 }
 
 export const updateReview = (review) => (dispatch) => {
   return ReviewApiUtil.updateReview(review)
     .then((payload) => {
       dispatch(receiveReview(payload))
-    })
+    }, errors => {
+      dispatch(receiveReviewErrors(errors.responseJSON))
+    });
 }
 
 export const deleteReview = (reviewId, gameId) => (dispatch) => {
   return ReviewApiUtil.deleteReview(reviewId, gameId)
     .then((reviews) => {
       dispatch(receiveAllReviews(reviews))
-    })
+    }, errors => {
+      dispatch(receiveReviewErrors(errors.responseJSON))
+    });
 }
