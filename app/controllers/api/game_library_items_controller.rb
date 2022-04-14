9d4f92362e3ae0710cbaa197739b6b5e8def83e1
Perry Xie <perry.xie1@gmail.com>
2022-04-13 23:13:38 -0700

    Remove user_id from permitted in game library item

--- app/controllers/api/game_library_items_controller.rb
+++ app/controllers/api/game_library_items_controller.rb
@@ -6,18 +6,19 @@ class Api::GameLibraryItemsController < ApplicationController
   end
 
   def create
-    @library_item = GameLibraryItem.new(library_params)
-    @library_items = User.find(library_params[:user_id]).game_library_items
-    if @library_item.save
+    library_item = GameLibraryItem.new(library_params)
+    library_item.user_id = current_user.id
+    @library_items = User.find(current_user.id).game_library_items
+    if library_item.save
       render :index
     else
-      render json: @library_item.errors.full_messages, status: 422
+      render json: library_item.errors.full_messages, status: 422
     end
   end
 
   private
   def library_params
-    params.require(:game_library_item).permit(:user_id, :game_id, :hours)
+    params.require(:game_library_item).permit(:game_id, :hours)
   end
 
 end
